0bcf86e10432e50bffdcd4f7941d8b22b3b11c10
damidaramola <damidaramola7@gmail.com>
2024-06-11 16:23:01 +0100

    add alternative solutions

--- arrays_and_hashing/containsDuplicate.py
+++ arrays_and_hashing/containsDuplicate.py
@@ -28,3 +28,27 @@ print(ans)
 # A set takes up  O(n) space
 # optimal solution 
 # I think this is the optimal solution
+
+#least efficient O(n^2) using nested loops, O(1) space complexity, no extra space/data structures needed
+
+# class Solution:
+#     def containsDuplicate(self, nums: list[int]) -> bool:
+#         for i in range(len(nums)):
+#             for j in range(i + 1, len(nums)):
+#                 if nums[i] == nums[j]:
+#                     return True
+#         return False
+#
+
+# Not optimal solution - sorting 
+
+# class Solution:
+#     def containsDuplicate(self, nums: list[int]) -> bool:
+#         nums.sort()  # Sort the list, O(n log n) time complexity
+#         for i in range(1, len(nums)):  # Iterate through the list starting from the second element
+#             if nums[i] == nums[i - 1]:  # Compare current element with the previous one
+#                 return True
+#         return False
+
+# TC = O(n log n)
+#SC = O(1) - sort operation is performed in place, and no additional data structures are used.
