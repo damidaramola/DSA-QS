9f0ff37406fd1c19314ea9f7ccf5b894fb51e397
damidaramola <damidaramola7@gmail.com>
2024-06-11 16:10:39 +0100

    add solution to contains duplicate

--- arrays_and_hashing/containsDuplicate.py
+++ arrays_and_hashing/containsDuplicate.py
@@ -1,1 +1,30 @@
 # Contains Duplicate
+
+
+class Solution:
+    def containsDuplicate(self, nums: list[int]) -> bool:
+        # create set to store the values in a loop
+        # for each loop check if num is already in the set 
+        # if yes return true else false and append that num to the set 
+        
+        store = set()
+
+        for num in nums:
+            if num in store:
+                return True 
+            store.add(num)
+        return False
+    
+    
+#nums = [1,2,2,5]
+nums =[1,2,3,4,6] 
+test = Solution()
+ans = test.containsDuplicate(nums)
+print(ans)
+
+#Time complexity
+
+#It is O(n) because we are looping through the array where n is number if elements in array
+# A set takes up  O(n) space
+# optimal solution 
+# I think this is the optimal solution
