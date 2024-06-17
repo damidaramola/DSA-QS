5d61b358a153f2c2a3bbe20cffc698b31f039539
damidaramola <damidaramola7@gmail.com>
2024-06-17 17:43:56 +0100

    solution to encode , decode

---
+++ Two_pointers/encodingDecoding.py
@@ -0,0 +1,46 @@
+class Solution:
+
+    def encode(self, strs: list[str]) -> str:
+        #["neet","code","love","you"]
+        #turn into a single string
+        # bf - create an empty string
+        #for every word, append each letter of that word to the string
+        # return string
+        # newstr = ""
+        # for word in strs:
+        #     for ch in word:
+        #         newstr.append(ch)
+        # print(newstr)
+        
+        res = ""
+        for word in strs:
+            res += str(len(word)) + "#" + word
+        return res
+        
+
+    def decode(self, s: str) -> list[str]:
+        #take the previous string, then split it back up into original string
+        # empty array
+        res = []
+        
+        i = 0
+        
+        while i < len(s):
+            j=i
+            while s[j] != '#':
+                j += 1
+            length = int(s[i:j])
+            res.append(s[j+1 : j+1+length])
+            i = j + 1 + length
+            
+        return res
+
+strs=["hold","on","nearly","there"]
+s = '4#hold2#on6#nearly5#there'
+# strs = ["go","in","the","hole"]
+# s= '2#go2#in3#the4#hole'
+test = Solution()
+# run = test.encode(strs)
+run = test.decode(s)
+       
+print(run)
