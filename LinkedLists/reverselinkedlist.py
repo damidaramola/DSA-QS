559b16d5fb3c5bd97e545bedde4b915625722c00
damidaramola <damidaramola7@gmail.com>
2024-07-22 19:46:40 +0100

    add solution to reverse linked list

---
+++ LinkedLists/reverselinkedlist.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, curr = None, head 
+
+        while curr:
+            next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next
+        return prev
+    
+    
+#space complexity - O(1) no extra space needed 
+# time complexity - O(n), we process each node in the list once
