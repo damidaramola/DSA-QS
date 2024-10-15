b841df85eb59a8f0f7ed301263adde9839443b89
damidaramola <damidaramola7@gmail.com>
2024-10-15 20:30:23 +0100

    add solution to pascals triangle

---
+++ Recursion/pascalsTriangle.py
@@ -0,0 +1,21 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+
+
+        # write base case 
+
+        if numRows == 0:
+            return []
+        if numRows ==1:
+            return [[1]]
+
+        prevRows = self.generate(numRows-1)
+        newRow = [1] * numRows
+
+        for i in range(1, numRows-1):
+            newRow[i] = prevRows[-1][i-1] + prevRows[-1][i]
+        prevRows.append(newRow)
+        return prevRows
+
+#Time complexity - O(numRows^2)
+# Space complexity - O(numRows)
